b4a10287e99e48c3e39842fb5da0350938a8e0e1
Matv1K <balahonovmatvej@gmail.com>
2024-11-18 07:13:54 +0300

    Fix structure

--- backend/utils/getTotalPrice.ts
+++
@@ -1,9 +0,0 @@
-import { CartItemI } from '../../types';
-
-const getTotalPrice = (items: CartItemI[]) => {
-  const totalPrice = items.reduce((total: number, item: CartItemI) => total + item.price * item.amount, 0);
-
-  return totalPrice;
-};
-
-export default getTotalPrice;
